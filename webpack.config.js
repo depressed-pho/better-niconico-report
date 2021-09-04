7e5d0f7867e410953ac5f8b6bf1e44087c8f2f76
PHO <pho@cielonegro.org>
2021-09-04 16:57:22 +0900

    Use different configurations for WebPack modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,123 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebPackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WebpackExtensionManifestPlugin = require('webpack-extension-manifest-plugin');
-const WebExtPlugin = require('web-ext-plugin');
+const WebpackExtensionManifestPlugin = require("webpack-extension-manifest-plugin");
+const WebExtPlugin = require("web-ext-plugin");
 
 /* fx-runner curently doesn't support NetBSD correctly. We need to
  * manually find the executable of firefox. */
 const firefoxBin = (() => {
-    const os = require('os');
+    const os = require("os");
     if (/netbsd/i.test(os.platform())) {
-        const which = require('which');
-        return which.sync('firefox');
+        const which = require("which");
+        return which.sync("firefox");
     }
     else {
-        return 'firefox';
+        return "firefox";
     }
 })();
 
-module.exports = {
-    mode: 'development',
-    entry: {
-        background: "./src/background.ts",
-        report: {
-            import: "./src/pages/report/report.ts",
-            filename: "assets/pages/[name]/[name].js"
+module.exports = (env, argv) => {
+    const prod = {
+        mode: "production",
+        devtool: "source-map",
+        optimization: {
+            usedExports: true // See https://webpack.js.org/guides/tree-shaking/
         }
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        assetModuleFilename: "assets/[name][ext]"
-    },
-    devtool: "inline-source-map", // Enable sourcemaps for debugging webpack's output.
-    resolve: {
-        extensions: [".ts", ".js"],
-        alias: {
-            'nicovideo': path.resolve(__dirname, 'lib')
-        }
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new CopyWebPackPlugin({
-            patterns: [
-                { from: "assets", to: "assets" },
-                {
-                    from: "src/pages",
-                    to: "assets/pages",
-                    filter: async (path) => {
-                        return /\.html$/.test(path);
-                    }
-                },
-            ]
-        }),
-        new MiniCssExtractPlugin({
-            filename: "assets/pages/[name]/[name].css"
-        }),
-        new WebpackExtensionManifestPlugin({
-            config: 'src/baseManifest.js',
-            pkgJsonProps: [
-                'version',
-                'description',
-            ]
-        }),
-        new WebExtPlugin({
-            sourceDir: path.resolve(__dirname, "dist"),
-            browserConsole: false,
-            firefox: firefoxBin
-        })
-    ],
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: 'ts-loader' },
-            {
-                test: /\.(eot|svg|ttf|woff)$/i,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "postcss-loader",
-                    "sass-loader"
-                ]
-            },
-            {
-                test: /\.html$/i,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: {
-                            esModule: true
-                        }
-                    }
-                ]
+    };
+    const dev = {
+        mode: "development",
+        devtool: "eval-cheap-module-source-map"
+    };
+    const common = {
+        entry: {
+            background: "./src/background.ts",
+            report: {
+                import: "./src/pages/report/report.ts",
+                filename: "assets/pages/[name]/[name].js"
             }
-        ]
+        },
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            assetModuleFilename: "assets/[name][ext]"
+        },
+        devtool: "inline-source-map", // Enable sourcemaps for debugging webpack's output.
+        resolve: {
+            extensions: [".ts", ".js"],
+            alias: {
+                'nicovideo': path.resolve(__dirname, 'lib')
+            }
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new CopyWebPackPlugin({
+                patterns: [
+                    { from: "assets", to: "assets" },
+                    {
+                        from: "src/pages",
+                        to: "assets/pages",
+                        filter: async (path) => {
+                            return /\.html$/.test(path);
+                        }
+                    },
+                ]
+            }),
+            new MiniCssExtractPlugin({
+                filename: "assets/pages/[name]/[name].css"
+            }),
+            new WebpackExtensionManifestPlugin({
+                config: 'src/baseManifest.js',
+                pkgJsonProps: [
+                    'version',
+                    'description',
+                ]
+            }),
+            new WebExtPlugin({
+                sourceDir: path.resolve(__dirname, "dist"),
+                browserConsole: false,
+                firefox: firefoxBin
+            })
+        ],
+        module: {
+            rules: [
+                { test: /\.tsx?$/, loader: 'ts-loader' },
+                {
+                    test: /\.(eot|svg|ttf|woff)$/i,
+                    type: 'asset/resource'
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "postcss-loader",
+                        "sass-loader"
+                    ]
+                },
+                {
+                    test: /\.html$/i,
+                    use: [
+                        {
+                            loader: "html-loader",
+                            options: {
+                                esModule: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    };
+    switch (argv.mode) {
+    case "development":
+        return merge(common, dev);
+
+    case "production":
+        return merge(common, prod);
+
+    default:
+        throw new Error("Unknown mode: " + argv.mode);
     }
 };
